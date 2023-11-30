1bdc28c1c43ab36c15e54009e5083959d6c801f9
ge-arty <george.arutinyan@gmail.com>
2023-12-01 00:03:35 +0400

    Add project files.

---
+++ BlazorApi/Services/LoginService.cs
@@ -0,0 +1,36 @@
+﻿// Services/LoginService.cs
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorApi.Models;
+
+
+public class LoginService
+{
+    private readonly HttpClient _httpClient;
+
+    public LoginService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public string apiUrl = "https://api.cubemaster.space:4351/api/Account/GetToken";
+
+    public async Task<string> GetToken(LoginModel loginModel)
+    {
+        var jsonContent = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(apiUrl, jsonContent);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var token = await response.Content.ReadAsStringAsync();
+            return token;
+        }
+
+       
+        return null;
+    }
+}
