e80ca7c6f2c8cdf4650229cba99657cb74fbe5d2
ge-arty <george.arutinyan@gmail.com>
2023-12-01 22:37:22 +0400

    push

---
+++ BlazorApi/Pages/Weather.razor.cs
@@ -0,0 +1,15 @@
+﻿//using BlazorApi.Models;
+//using System.Net.Http.Json;
+
+//namespace BlazorApi.Pages
+//{
+//    public partial class Weather
+//    {
+//        private WeatherForecastModel[]? forecasts;
+
+//        protected override async Task OnInitializedAsync()
+//        {
+//            forecasts = await Http.GetFromJsonAsync<WeatherForecastModel[]>("sample-data/weather.json");
+//        }
+//    }
+//}
